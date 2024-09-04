36b1b8a0a59609f0061ff8f2498c2adde920bfa9
AbdullRafay <156018415+AbdullRafay@users.noreply.github.com>
2024-09-04 07:03:53 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,61 @@
-const input=document.getElementById('input');
+const input = document.getElementById("input");
 
-input.addEventListener('submit', function(event){
-    event.preventDefault();
+input.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  var fname = document.getElementById("fname").value;
+  var lname = document.getElementById("lname").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var tableBody = document.getElementById("tablebody");
+  var searchKey=document.getElementById("inpkey").value;
+  var searchInp=document.getElementById("searchinp").value;
+var arr=[];
+  var obj = {
+    fname: fname,
+    lname: lname,
+    email: email,
+    password: password,
+  };
+  arr.push({ ...obj });
+
+  fname.value=" ";
+lname.value=" ";
+email.value=" ";
+password.value=" ";
 
 
- var fname=document.getElementById('fname').value;
- var lname=document.getElementById('lname').value;
- var email=document.getElementById('email').value;
- var password=document.getElementById('password').value;
 
 
- var obj={
-    fname : fname,
-    lname : lname,
-    email : email,
-    password :password,
-    
+  function tableInput(){
+ 
 
- }
-console.log(obj);
-})
+   for (var i = 0; i < arr.length; i++) {
+      var x = arr[i];
+      if(x[searchInp] == searchKey){
+         console.log(x);
+      }
+
+  }
+}
+
+  function renderTable() {
+    tableBody.innerHTML = "";
+    for (var i = 0; i < arr.length; i++) {
+      var x = arr[i];
+
+      tableBody.innerHTML += `
+       <tr>
+       <td>${x.fname}</td>
+       <td>${x.lname}</td>
+       <td>${x.email}</td>
+       <td>${x.password}</td>
+   
+       </tr>`;
+    }
+  }
+
+  console.log(obj);
+  renderTable();
+  tableInput();
+});
